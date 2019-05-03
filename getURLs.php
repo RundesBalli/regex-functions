329b9dadaf954c8bf990b4f432c039535f7ec99c
RundesBalli <rundesballi@rundesballi.com>
2019-05-03 18:53:33 +0200

    Add file to repository

---
+++ getURLs.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Function to extract URLs from a text. Additionally it is checked if the URL is valid (Check DNS if there is an A-Record)
+ * 
+ * @param string The text.
+ * 
+ * @return array/NULL An array is returned if valid URLs are in the text, if not NULL is returned.
+ */
+function getURLs($text) {
+  $regex = '/(^|\s)((https?:\/\/)?([\w-]+)(\.[a-z-]{2,})(\.[a-z-]{2,})?(\/\S*)?)/i';
+  preg_match_all($regex, $text, $matches);
+  $urls = array();
+  foreach($matches[0] as $key => $value) {
+    $url = trim($value);
+    $dnsurl = parse_url($url, PHP_URL_HOST);
+    if(!empty($dnsurl)) {
+      if(checkdnsrr($dnsurl, "A")) {
+        $urls[] = $url;
+      }
+    }
+  }
+  if(empty($urls)) {
+    return NULL;
+  } else {
+    return $urls;
+  }
+}
+?>
